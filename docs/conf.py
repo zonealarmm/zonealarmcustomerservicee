26735001aaa1f297a981cccc424970191f4358b7
zonealarmm <gjjfgrfht@outlook.com>
2026-08-06 03:25:26 -0700

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,15 @@
+project = 'ZoneAlarm'
+author = 'Your Name'
+release = '1.0'
+
+extensions = []
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
+html_js_files = [
+    'chatbot.js',
+]
+html_favicon = '.png'
